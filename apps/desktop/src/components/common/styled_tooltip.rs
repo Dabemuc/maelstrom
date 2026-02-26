9130e6b6a743471d83545c3a2d9664ad7848273e
Dabemuc <daniel.benner02@gmail.com>
2026-02-26 20:28:55 +0100

    [desktop] custom tooltip styling

--- apps/desktop/src/components/common/styled_tooltip.rs
+++ apps/desktop/src/components/common/styled_tooltip.rs
@@ -1,7 +1,8 @@
 use crate::Message;
-use iced::Element;
+use iced::border::Radius;
 use iced::widget::tooltip::Position;
 use iced::widget::{Tooltip, container, text};
+use iced::{Border, Element};
 
 /// Ergonomic tooltip wrapper that works with any widget and string-like content.
 pub fn styled_tooltip<'a, W, S>(
@@ -15,7 +16,21 @@ where
 {
     Tooltip::new(
         content.into(),
-        container(text(tooltip_content.into())),
+        container(text(tooltip_content.into()))
+            .padding(10)
+            .style(|theme: &iced::Theme| {
+                let palette = theme.extended_palette();
+                container::Style {
+                    background: Some(palette.background.weakest.color.into()),
+                    text_color: Some(palette.background.weak.text),
+                    border: Border {
+                        color: palette.secondary.base.color,
+                        radius: Radius::new(4),
+                        width: 1.0,
+                    },
+                    ..Default::default()
+                }
+            }),
         position,
     )
 }
